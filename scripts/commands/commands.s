57ef92254404b8bf50975f985e391592e28cb68e
Hunter Lee <narendly@gmail.com>
2016-11-30 23:19:23 -0500

    Add TURTLE_FILLCELL and fix bug

--- scripts/commands/commands.s
+++ scripts/commands/commands.s
@@ -66,6 +66,11 @@ westf:
     
 endforward:
 
+    #draw turtle at the new location
+    j TURTLE_FILLCELL
+    nop
+    ENDTURTLE_FILLCELL:
+
     #draw here
     j DRAW_FORWARD
     nop
@@ -112,6 +117,11 @@ westb:
 
     
 endbackward:
+
+    #draw turtle at the new location
+    j TURTLE_FILLCELL
+    nop
+    ENDTURTLE_FILLCELL:
 
     #draw here
     j DRAW_BACKWARD:
@@ -368,7 +378,7 @@ enddrawbackward:
     j ENDDRAW_BACKWARD
 
 
-###FILLCELL: svga wrapper for svga per cell
+###FILLCELL: svga wrapper for svga per cell for lines
 DRAW_FILLCELL:
     #Put the svga snippet here
     #Use $6 for x, $7 for y, $13 for color
@@ -446,7 +456,94 @@ DRAW_FILLCELL:
     j ENDDRAW_FILLCELL
 
     
+###TURTLE_FILLCELL: svga wrapper for svga per cell for rendering turtle
+TURTLE_FILLCELL:
+    #Put the svga snippet here
+    #Use $10 for x, $11 for y, use DMEM 
+
+    #Initialize temp registers
+    add $20, $0, $0
+    add $21, $0, $0
+    add $22, $0, $0
+    add $23, $0, $0
+    add $24, $0, $0
+    add $25, $0, $0
+    add $26, $0, $0
     
+    #calculate top left starting pixel index
+    #and store it in $20
+    #(640*row) + col + 80 = (640*y) + x + 80
+    #$21 = 640
+    #row = $11, col = $10 !!
+    addi $21, $0, 640
+    mul $20, $21, $11
+    add $20, $20, $10
+    addi $20, $20, 80
+    
+    #$22 = 15 (go from 0 to 14)
+    #$23, $24 are loop variables
+    addi $22, $0, 15
+    addi $23, $0, 0
+    addi $24, $0, 0
+    
+    
+    #$25: color value for turtle from DMEM
+    #$26: offset in DMEM [0, 254]
+    
+    loopcol1t:
+        
+        beq $23, $22, endloop1t #$22=15
+        
+        #get the index for this iteration
+        #$24 is the temporary index
+        add $24, $20, $23
+        
+        
+        #color it
+        
+        #first, load from DMEM using DMEM offset
+        lw $25, 0($26)
+        
+        sw $25, 0($24)
+        #svga $25, 0($24) #TODO: change to svga! : hl130 
+        
+        #increment index
+        addi $23, $23, 1
+        
+        #increment DMEM offset
+        addi $26, $26, 1
+
+        j loopcol1t
+        
+    
+    
+    
+    endloop1t:
+    
+        #ran this outer loop 15 times? then you're done!
+        beq $24, $22, endloop2t
+        
+        #first, increment the outer loop variable
+        addi $24, $24, 1
+    
+        #one iteration is done, so add 640 to $20
+        add $20, $20, $21
+        
+        #now set loop var to 0 and loop again 15 times
+        add $23, $0, $0 #inner loop var 0
+        j loopcol1t
+    
+    
+    endloop2t:
+        #cell all filled, clear the variables and return
+        add $20, $0, $0
+        add $21, $0, $0
+        add $22, $0, $0
+        add $23, $0, $0
+        add $24, $0, $0
+    
+    j ENDTURTLE_FILLCELL
+
     
     
     
@@ -455,12 +552,16 @@ LEFTROTATE:
     #$12 has current direction
     
     #process argument
-    jal modfour
+    j modfour_l
+    nop
+    endmodfour_l:
     
     sub $12, $12, $4 #subtract
     
     #if $12 is negative, add 4
-    blt $12, $0, addfour
+    blt $12, $0, addfour_l
+    nop
+    endaddfour_l:
     
     jr $31 # return after leftrotate
 
@@ -470,14 +571,18 @@ RIGHTROTATE:
     #$12 has current direction
     
     #process argument
-    jal modfour
+    j modfour_r
+    nop
+    endmodfour_r:
     
     add $12, $12, $4 #add
     
     #if $12 is greater than 4, we adjust the number
     #subtract 4 and do a blt
     addi $12, $12, -4
-    blt $12, $0, addfour
+    blt $12, $0, addfour_r
+    
+    endaddfour_r:
     
     jr $31 # return after rightrotate
     
@@ -485,7 +590,7 @@ RIGHTROTATE:
     
     
 ###ARGUMENT PROCESSING FOR ROTATE
-modfour:
+modfour_l:
     #Get modulo 4 of the argument
     #and stores it back into $4
     #$6-$9 are temp registers
@@ -510,12 +615,45 @@ modfour:
     #addi $8, $0, 0
     #addi $9, $0, 0
     
-    jr $31 #return to rotate
+    j endmodfour_l #return to rotate
 
+###ARGUMENT PROCESSING FOR ROTATE
+modfour_r:
+    #Get modulo 4 of the argument
+    #and stores it back into $4
+    #$6-$9 are temp registers
+    #assumes multdiv does not have mod implemented
+    
+    #initialize
+    addi $6, $0, 4 #$6 = 4
+    addi $7, $0, 0
+    addi $8, $0, 0
+    addi $9, $0, 0
+    
+    div $7, $1, $6 
+    mul $8, $6, $7
+    sub $9, $4, $8
+    
+    #store it back to $4
+    add $4, $0, $9
+
+    #clear registers--redundant?
+    #addi $6, $0, 4 #$6 = 4
+    #addi $7, $0, 0
+    #addi $8, $0, 0
+    #addi $9, $0, 0
+    
+    j endmodfour_r #return to rotate
+    
     
 ###ADD 4 TO RETURN TO [0, 3] RANGE
-addfour:
+addfour_l:
     addi $12, $12, 4
-    jr $31
     
+    j endaddfour_l
     
+###ADD 4 TO RETURN TO [0, 3] RANGE
+addfour_r:
+    addi $12, $12, 4
+    
+    j endaddfour_r
